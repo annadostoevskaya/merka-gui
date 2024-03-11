23f4d41f8bb9eef3ea9976700bd91d0195b72b31
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2024-03-11 05:10:38 +0500

    feature: fix w/h sizes

--- src/main.py
+++ src/main.py
@@ -4,14 +4,15 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 class Root(QMainWindow):
-    def __init__(self):
+    def __init__(self, w, h):
         super(Root, self).__init__()
+        self.setFixedSize(w, h)
         uic.loadUi('views/layout.ui', self)
         self.show()
 
 def main():
     app = QApplication([])
-    root = Root()
+    root = Root(1300, 800)
     sys.exit(app.exec())
 
 if __name__ == '__main__':
